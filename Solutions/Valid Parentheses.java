37d5eb2e453921c535cfaa9a8916a8e93ee2db4a
Sérgio Brito <64617586+sergiobriito@users.noreply.github.com>
2023-06-18 14:37:54 -0300

    .

---
+++ Solutions/Valid Parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> matching = new HashMap();
+        matching.put('(', ')');
+        matching.put('[', ']');
+        matching.put('{', '}');
+        
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (matching.containsKey(c)) { 
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                
+                char previousOpening = stack.pop();
+                if (matching.get(previousOpening) != c) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.empty();
+    }
+}
